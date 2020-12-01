9647ef5f0fcdfb9f0566c993d480b6669d6c6be8
Matt Mullins <mmullins@mmlx.us>
2020-11-30 23:14:48 -0800

    day 01: factor out file I/O and test the results

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,11 +1,13 @@
 use std::io::BufRead;
 
 fn main() {
-    let stdin = std::io::stdin();
-    let lock = stdin.lock();
-    let buffered = std::io::BufReader::new(lock);
+    do_main("inputs/day_01.txt");
+}
+
+fn do_main(filename: &str) {
+    let file = std::fs::File::open(filename).expect("could not open the input");
     let mut expenses = Vec::new();
-    for line in buffered.lines() {
+    for line in std::io::BufReader::new(file).lines() {
         if let Ok(line) = line {
             if let Ok(expense) = line.as_str().trim().parse::<isize>() {
                 expenses.push(expense);
@@ -13,10 +15,14 @@ fn main() {
         }
     }
 
+    let mut part1 = None;
+    let mut part2 = None;
+
     'out: for i in 0..expenses.len() {
         for j in 0..expenses.len() {
             if i != j && expenses[i] + expenses[j] == 2020 {
                 println!("Answer is {}", expenses[i] * expenses[j]);
+                part1 = Some(expenses[i] * expenses[j]);
                 break 'out;
             }
         }
@@ -33,9 +39,21 @@ fn main() {
                 }
                 if expenses[i] + expenses[j] + expenses[k] == 2020 {
                     println!("Answer is {}", expenses[i] * expenses[j] * expenses[k]);
+                    part2 = Some(expenses[i] * expenses[j] * expenses[k]);
                     break 'out2;
                 }
             }
         }
+    }
+
+    assert_eq!(part1, Some(1019904));
+    assert_eq!(part2, Some(176647680));
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn main() {
+        super::do_main("../inputs/day_01.txt");
     }
 }
