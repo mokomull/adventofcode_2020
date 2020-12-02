f17084096568798caee6cb296a32b7fbb9946035
Matt Mullins <mmullins@mmlx.us>
2020-12-01 21:20:59 -0800

    day 02: check part 2 with iterators
    This is wrong, according to the site.
    
    Except I factored it all out, and it succeeds the tests for their
    examples.  Something's odd here.

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -41,4 +41,45 @@ fn do_main(filename: &str) {
         })
         .count();
     println!("part 1: {}", part1);
+
+    let part2 = passwords
+        .iter()
+        .filter(|&password| part2_check(password))
+        .count();
+    println!("part 2: {}", part2);
+}
+
+fn part2_check(password: &Password) -> bool {
+    password
+        .password
+        .chars()
+        .enumerate()
+        .filter(|&(i, c)| c == password.c && (i >= password.low - 1 && i <= password.high - 1))
+        .count()
+        == 1
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn part2_check() {
+        assert!(super::part2_check(&super::Password {
+            low: 1,
+            high: 3,
+            c: 'a',
+            password: "abcde".into(),
+        }));
+        assert!(!super::part2_check(&super::Password {
+            low: 1,
+            high: 3,
+            c: 'b',
+            password: "cdefg".into(),
+        }));
+        assert!(!super::part2_check(&super::Password {
+            low: 2,
+            high: 9,
+            c: 'c',
+            password: "ccccccccc".into(),
+        }));
+    }
 }
