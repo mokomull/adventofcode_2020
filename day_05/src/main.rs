cc729ec888309e0fe7bb4fe634735b4f97171e32
Matt Mullins <mmullins@mmlx.us>
2020-12-04 21:13:41 -0800

    day 05: split out the binary parsing and test it
    This actually passes for the examples they gave...

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -10,31 +10,47 @@ fn do_main(filename: &str) {
         .lines()
         .map(|line| {
             let line = line.expect("could not read line");
-            let row = line
-                .chars()
-                .take(7)
-                .map(|c| match c {
-                    'F' => '0',
-                    'B' => '1',
-                    x => panic!("unexpected front/back: {}", x),
-                })
-                .collect::<String>();
-            let column = line
-                .chars()
-                .skip(7)
-                .take(3)
-                .map(|c| match c {
-                    'L' => '0',
-                    'R' => '1',
-                    x => panic!("unexpected left/right: {}", x),
-                })
-                .collect::<String>();
-            (
-                u32::from_str_radix(&row, 2).expect("could not parse row"),
-                u32::from_str_radix(&column, 2).expect("could not parse column"),
-            )
+            seat_from_directions(&line)
         })
         .map(|(row, column)| row * column)
         .max();
     dbg!(part1);
+}
+
+fn seat_from_directions(directions: &str) -> (u32, u32) {
+    let row = directions
+        .chars()
+        .take(7)
+        .map(|c| match c {
+            'F' => '0',
+            'B' => '1',
+            x => panic!("unexpected front/back: {}", x),
+        })
+        .collect::<String>();
+    let column = directions
+        .chars()
+        .skip(7)
+        .take(3)
+        .map(|c| match c {
+            'L' => '0',
+            'R' => '1',
+            x => panic!("unexpected left/right: {}", x),
+        })
+        .collect::<String>();
+    (
+        u32::from_str_radix(&row, 2).expect("could not parse row"),
+        u32::from_str_radix(&column, 2).expect("could not parse column"),
+    )
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn part1() {
+        assert_eq!(seat_from_directions("BFFFBBFRRR"), (70, 7));
+        assert_eq!(seat_from_directions("FFFBBBFRRR"), (14, 7));
+        assert_eq!(seat_from_directions("BBFFBBFRLL"), (102, 4));
+    }
 }
