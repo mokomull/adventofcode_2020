1584bbf3a545c8f377f03900932f6f1d56913cc6
Matt Mullins <mmullins@mmlx.us>
2020-12-07 00:13:24 -0800

    day 07: walk the bags from the bottom up
    I initially got this wrong because I had (count * known_bag) instead of
    (count * (1 + known_bag)) -- so the outer bag at any layer was not
    counted as a bag at all!
    
    There is no way I'll be able to understand what I did next time I have
    to read this code, but in brief: it builds a HashMap of bags whose
    contents we know, and keeps going until we have figured out what goes
    inside a shiny gold bag.

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::io::BufRead;
 
 use itertools::Itertools;
@@ -35,6 +35,37 @@ fn do_main(filename: &str) {
 
     let part1 = can_contain_gold.len();
     dbg!(part1);
+
+    let mut bags_inside = HashMap::new();
+
+    while !bags_inside.contains_key(&"shiny gold".to_owned()) {
+        for (outer, inners) in &input {
+            if bags_inside.contains_key(&outer) {
+                continue;
+            }
+
+            let cost = inners
+                .iter()
+                .map(|(count, kind)| {
+                    bags_inside
+                        .get(kind)
+                        .map(|&known_bag| count * (1usize + known_bag))
+                })
+                .fold(Some(0), |prev, cost_of_one_inner_bag| {
+                    match (prev, cost_of_one_inner_bag) {
+                        (Some(running_cost), Some(next_bag)) => Some(running_cost + next_bag),
+                        _ => None,
+                    }
+                });
+
+            if let Some(this_cost) = cost {
+                bags_inside.insert(outer, this_cost);
+            }
+        }
+    }
+
+    let part2 = bags_inside.get(&"shiny gold".to_owned()).unwrap();
+    dbg!(part2);
 }
 
 fn parse_line(line: &str) -> (String, Vec<(usize, String)>) {
