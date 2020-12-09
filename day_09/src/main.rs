8928d74a7f3d78721cface9a1600085efe8e39df
Matt Mullins <mmullins@mmlx.us>
2020-12-08 21:15:17 -0800

    day 09: try to be clever and keep running sums
    This, so far, hasn't worked out.

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -28,4 +28,26 @@ fn do_main(filename: &str) {
     }
 
     dbg!(part1);
+
+    let mut part2 = None;
+
+    let mut sums = input.clone();
+
+    'out: for distance in 0..sums.len() {
+        for (i, sum) in sums.iter_mut().enumerate() {
+            if sum == part1.unwrap() {
+                part2 = Some(input[i] + input[i + distance]);
+                break 'out;
+            }
+
+            if let Some(new) = input.get(i + distance + 1) {
+                *sum = *sum + new;
+            }
+        }
+
+        // the last one will always no longer have a pairing
+        sums.pop();
+    }
+
+    dbg!(part2);
 }
