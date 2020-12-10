dd38f91d5347477fa66d33d2b316939ce4a2fa78
Matt Mullins <mmullins@mmlx.us>
2020-12-09 21:20:12 -0800

    day 10: multiply the number of combinations
    This produces garbage so far.

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -9,13 +9,15 @@ fn do_main(filename: &str) {
         .map(|s| s.parse().expect("not an integer"))
         .collect();
 
+    // we always start with 0
+    input.push(0);
     input.sort();
     let input = input;
 
     let mut one = 0;
     let mut three = 0;
 
-    for (lower, upper) in std::iter::once(&0).chain(input.iter()).zip(input.iter()) {
+    for (lower, upper) in input.iter().zip(input.iter().skip(1)) {
         match upper - lower {
             0 => panic!("zero: {}", lower),
             1 => one += 1,
@@ -27,4 +29,15 @@ fn do_main(filename: &str) {
 
     let part1 = one * (three + 1); // + 1 due to the "built in" one being one-higher
     dbg!(part1);
+
+    let mut ways = 1;
+    for i in 0..(input.len() - 1) {
+        ways *= input
+            .iter()
+            .skip(i + 1)
+            .take(3)
+            .filter(|&next| *next <= input[i] + 3)
+            .count();
+    }
+    dbg!(ways);
 }
