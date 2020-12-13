2f0c70316462595b2a574f23fed90ba6cd305c29
Matt Mullins <mmullins@mmlx.us>
2020-12-13 14:22:21 -0800

    day 12: almost exactly the same but move waypoint

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -58,6 +58,59 @@ fn do_main(filename: &str) {
 
     let part1 = x.abs() + y.abs();
     dbg!(part1);
+
+    // almost exactly the same, except most movements move the waypoint, not the
+    // ship.
+    // x and y are the ship's position.
+    let mut x = 0;
+    let mut y = 0;
+
+    // The waypoint starts at 10 units east, 1 north
+    let mut dx = 10;
+    let mut dy = 1;
+
+    for i in &input {
+        match i {
+            North(d) => dy += *d as i32,
+            South(d) => dy -= *d as i32,
+            East(d) => dx += *d as i32,
+            West(d) => dx -= *d as i32,
+            Forward(d) => {
+                x += *d as i32 * dx;
+                y += *d as i32 * dy;
+                // "The waypoint stays 10 units east and 1 unit north of the
+                // ship.", so we don't need to fix-up the waypoint's location to
+                // be relative to where we just put the ship.
+            }
+            Left(d) => {
+                // this problem gets harder if we get off the axes
+                assert!(*d % 90 == 0);
+
+                for _ in 0..(*d / 90) {
+                    // [1 0] -> [0 -1] so the transformation is [0 -1] * [dx]
+                    // [0 1]    [1  0]                          [1  0]   [dy]
+                    let newdx = -dy;
+                    let newdy = dx;
+                    dx = newdx;
+                    dy = newdy;
+                }
+            }
+            Right(d) => {
+                assert!(*d % 90 == 0);
+                for _ in 0..(*d / 90) {
+                    // same thing except [1 0] -> [0  1]
+                    //                   [0 1]    [-1 0]
+                    let newdx = dy;
+                    let newdy = -dx;
+                    dx = newdx;
+                    dy = newdy;
+                }
+            }
+        }
+    }
+
+    let part2 = x.abs() + y.abs();
+    dbg!(part2);
 }
 
 enum Instruction {
