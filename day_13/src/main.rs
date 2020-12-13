46775434b86c3c9f9065bd4c20fe43fb889d97c9
Matt Mullins <mmullins@mmlx.us>
2020-12-13 15:32:22 -0800

    day 13: implement the Extended Euclidean Algorithm
    This is the first step toward solving the Chinese Remainder Theorem on
    the inputs.

--- day_13/src/main.rs
+++ day_13/src/main.rs
@@ -50,3 +50,27 @@ fn do_main(filename: &str) {
         .expect("no suitable timestamp was found");
     dbg!(part2);
 }
+
+fn extended_euclidean(a: i64, b: i64) -> (i64, i64) {
+    extended_euclidean_inner(a, b, 1, 0, 0, 1)
+}
+
+fn extended_euclidean_inner(a: i64, b: i64, s0: i64, s1: i64, t0: i64, t1: i64) -> (i64, i64) {
+    if b == 0 {
+        assert_eq!(a, 1); // this algorithm makes no sense if gcd(orig_a, orig_b) != 1
+        return (s0, t0);
+    }
+
+    let q = a / b;
+    extended_euclidean_inner(b, a - q * b, s1, s0 - q * s1, t1, t0 - q * t1)
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn euclidean() {
+        // gcd(240, 23) == 1
+        // 7 * 240 + -73 * 23 == 1
+        assert_eq!(super::extended_euclidean(240, 23), (7, -73));
+    }
+}
