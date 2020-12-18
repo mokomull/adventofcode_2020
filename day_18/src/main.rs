9b236e52d2fb9b6308fd6058b44be9a1ac78c2a1
Matt Mullins <mmullins@mmlx.us>
2020-12-17 21:30:27 -0800

    day 18: use the Shunting Yard algorithm
    This turns it into RPN, which in turn is much easier to evaluate.
    
    This crashes, though.

--- day_18/src/main.rs
+++ day_18/src/main.rs
@@ -10,8 +10,62 @@ fn do_main(filename: &str) {
     let input: Vec<Vec<Token>> = read_lines_from_file(filename)
         .map(|line| parse_line(&line))
         .collect();
+
+    let part1: i32 = input.iter().map(|line| evaluate(&line)).sum();
+    dbg!(part1);
 }
 
+fn evaluate(tokens: &[Token]) -> i32 {
+    let mut rpn_queue = Vec::new();
+    let mut op_stack = Vec::new();
+
+    for token in tokens {
+        match token {
+            t @ Integer(_) => rpn_queue.push(t),
+            t @ Plus | t @ Asterisk => {
+                while !op_stack.is_empty() && op_stack.last().unwrap() != &&LeftParen {
+                    rpn_queue.push(op_stack.pop().unwrap());
+                }
+                op_stack.push(t);
+            }
+            LeftParen => op_stack.push(&LeftParen),
+            RightParen => {
+                while op_stack.last().unwrap() != &&LeftParen {
+                    rpn_queue.push(op_stack.pop().unwrap());
+                }
+                assert_eq!(op_stack.pop(), Some(&LeftParen));
+            }
+        }
+    }
+    assert!(op_stack.is_empty());
+
+    let mut stack: Vec<i32> = Vec::new();
+
+    for token in rpn_queue {
+        match token {
+            Integer(x) => stack.push(*x),
+            Plus => {
+                let right = stack.pop().unwrap();
+                let left = stack.pop().unwrap();
+                stack.push(left + right);
+            }
+            Asterisk => {
+                let right = stack.pop().unwrap();
+                let left = stack.pop().unwrap();
+                stack.push(left * right);
+            }
+            LeftParen | RightParen => panic!("we should have killed all the parens by now"),
+        }
+    }
+
+    if let Some(x) = stack.last() {
+        *x
+    } else {
+        panic!("Unexpected stack: {:?}", stack);
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
 enum Token {
     Integer(i32),
     Plus,
