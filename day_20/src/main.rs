798a58c2ad9ef8f088025bf36dc001826945ae6f
Matt Mullins <mmullins@mmlx.us>
2020-12-19 21:40:12 -0800

    day 20: keep track of which edges matched
    This still only gets 122 total edges matched in the whole input.  I
    would have expected this to be close to 144*4.

--- day_20/src/main.rs
+++ day_20/src/main.rs
@@ -1,5 +1,7 @@
 use prelude::*;
 
+use Edge::*;
+
 fn main() {
     do_main("inputs/day_20.txt");
 }
@@ -7,31 +9,51 @@ fn main() {
 fn do_main(filename: &str) {
     let tiles = parse_tiles(read_lines_from_file(filename));
 
-    let mut edge_counts = HashMap::<usize, usize>::new();
+    let mut edge_counts = HashMap::<(usize, Edge), usize>::new();
     for (t1, t2) in tiles.iter().tuple_combinations() {
-        for (e1, e2) in [t1.left(), t1.right(), t1.top(), t1.bottom()]
-            .iter()
-            .zip([t2.left(), t2.right(), t2.top(), t2.bottom()].iter())
-        {
-            if e1 == e2 {
-                *edge_counts.entry(t1.id).or_default() += 1;
-                *edge_counts.entry(t2.id).or_default() += 1;
+        for (e1, e2) in [
+            (Left, t1.left()),
+            (Right, t1.right()),
+            (Top, t1.top()),
+            (Bottom, t1.bottom()),
+        ]
+        .iter()
+        .zip(
+            [
+                (Left, t2.left()),
+                (Right, t2.right()),
+                (Top, t2.top()),
+                (Bottom, t2.bottom()),
+            ]
+            .iter(),
+        ) {
+            if e1.1 == e2.1 {
+                *edge_counts.entry((t1.id, e1.0)).or_default() += 1;
+                *edge_counts.entry((t2.id, e2.0)).or_default() += 1;
             }
-            let mut e2 = e2.clone();
-            e2.reverse();
-            if e1 == &e2 {
-                *edge_counts.entry(t1.id).or_default() += 1;
-                *edge_counts.entry(t2.id).or_default() += 1;
+            let mut reversed = e2.1.clone();
+            reversed.reverse();
+            if e1.1 == reversed {
+                *edge_counts.entry((t1.id, e1.0)).or_default() += 1;
+                *edge_counts.entry((t2.id, e2.0)).or_default() += 1;
             }
         }
     }
-    dbg!(edge_counts);
+    dbg!(edge_counts.len());
 }
 
 #[derive(Debug)]
 struct Tile {
     id: usize,
     image: Vec<Vec<bool>>,
+}
+
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
+enum Edge {
+    Left,
+    Right,
+    Top,
+    Bottom,
 }
 
 fn parse_tiles<I>(input: I) -> Vec<Tile>
