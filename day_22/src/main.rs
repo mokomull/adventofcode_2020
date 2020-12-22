bd2f6832784fc532360c1b38bcb60d955e4720f0
Matt Mullins <mmullins@mmlx.us>
2020-12-21 21:37:33 -0800

    day 22: try to implement recursive combat

--- day_22/src/main.rs
+++ day_22/src/main.rs
@@ -7,8 +7,9 @@ fn main() {
 }
 
 fn do_main(filename: &str) {
-    let mut decks =
+    let orig_decks =
         parse_lines(&mut read_lines_from_file(filename).chain(std::iter::once("".into())));
+    let mut decks = orig_decks.clone();
 
     while decks.iter().all(|deck| !deck.is_empty()) {
         let round: Vec<u32> = decks
@@ -33,6 +34,61 @@ fn do_main(filename: &str) {
         .map(|(card, i)| *card * i)
         .sum();
     dbg!(part1);
+
+    let mut decks = orig_decks.clone();
+    let mut seen = HashSet::new();
+    while decks.iter().all(|deck| !deck.is_empty()) {
+        recursive_combat(&mut decks, &mut seen);
+    }
+
+    let part2: u32 = decks
+        .iter()
+        .flat_map(|d| d.iter().rev())
+        .zip(1..)
+        .map(|(card, i)| *card * i)
+        .sum();
+    dbg!(part2);
+}
+
+fn recursive_combat(
+    decks: &mut Vec<VecDeque<u32>>,
+    seen: &mut HashSet<Vec<VecDeque<u32>>>,
+) -> usize {
+    if seen.contains(decks) {
+        return 0;
+    }
+
+    let round: Vec<u32> = decks
+        .iter_mut()
+        .map(|deck| deck.pop_front().unwrap())
+        .collect();
+
+    let should_recurse = decks
+        .iter()
+        .zip(round.iter())
+        .all(|(deck, count)| deck.len() >= *count as usize);
+
+    let winner = if should_recurse {
+        let mut new_decks = decks
+            .iter()
+            .zip(&round)
+            .map(|(deck, count)| deck.iter().take(*count as usize).cloned().collect())
+            .collect();
+        recursive_combat(&mut new_decks, seen)
+    } else {
+        round.iter().position_max().unwrap()
+    };
+
+    let loser = match winner {
+        0 => 1,
+        1 => 0,
+        x => panic!("who's player {}", x),
+    };
+
+    decks[winner].push_back(round[winner]);
+    decks[winner].push_back(round[loser]);
+
+    winner
 }
 
 fn parse_lines<I>(input: I) -> Vec<VecDeque<u32>>
