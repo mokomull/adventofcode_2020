426be21418adeb489ec8c6f5e0fce6977b5c4173
Matt Mullins <mmullins@mmlx.us>
2020-12-23 01:00:09 -0800

    day 23: it didn't need to be doubly-linked at all

--- day_23/src/main.rs
+++ day_23/src/main.rs
@@ -20,7 +20,6 @@ fn main() {
 
 #[derive(Clone, Default, Debug)]
 struct Cup {
-    previous: usize,
     next: usize,
 }
 
@@ -29,9 +28,7 @@ fn setup_cups(input: &[usize]) -> Vec<Cup> {
 
     for (&i, &j) in input.iter().zip(input.iter().skip(1)) {
         cups[i].next = j;
-        cups[j].previous = i;
     }
-    cups[input[0]].previous = *input.last().unwrap();
     cups[*input.last().unwrap()].next = input[0];
 
     cups
@@ -56,7 +53,6 @@ fn run(iterations: i32, input: &[usize]) -> Vec<usize> {
         }
         let next_after_this_round = cups[end_of_range].next;
         cups[current_cup].next = next_after_this_round;
-        cups[next_after_this_round].previous = current_cup;
 
         let mut destination = current_cup - 1;
         if destination < min {
@@ -72,9 +68,7 @@ fn run(iterations: i32, input: &[usize]) -> Vec<usize> {
         // now put the range back in between destination and its successor
         let next_after_destination = cups[destination].next;
         cups[destination].next = beginning_of_range;
-        cups[beginning_of_range].previous = destination;
         cups[end_of_range].next = next_after_destination;
-        cups[next_after_destination].previous = end_of_range;
 
         current_cup = cups[current_cup].next;
     }
