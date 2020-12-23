b77f7689b9b596d0ea779c8265ef1a815f343767
Matt Mullins <mmullins@mmlx.us>
2020-12-23 00:26:27 -0800

    day 23: model the cups as a doubly-linked-list
    Linked-lists are famously hard to implement in Rust, but since the input
    is a dense set of small integers, we can use indices in a Vec instead of
    ever actually needing a reference to any of them.
    
    Hat tip to Patrick Bogen for mentioning this.

--- day_23/src/main.rs
+++ day_23/src/main.rs
@@ -1,47 +1,69 @@
-use std::collections::VecDeque;
-
 fn main() {
     let input = [1, 5, 6, 7, 9, 4, 8, 2, 3];
 
-    let mut cups: VecDeque<i32> = input.iter().cloned().collect();
-    run(100, &mut cups);
+    let cups = run(100, &input);
 
     dbg!(&cups);
-    assert_eq!(&cups, &[7, 3, 4, 9, 6, 1, 8, 2, 5]);
+    assert_eq!(&cups, &[8, 2, 5, 7, 3, 4, 9, 6]);
+    return;
 
-    let mut cups: VecDeque<i32> = input.iter().cloned().collect();
-    for i in 10..=1_000_000 {
-        cups.push_back(i);
-    }
-    run(10_000_000, &mut cups);
-
-    while *cups.back().unwrap() != 1 {
-        let cup = cups.pop_front().unwrap();
-        cups.push_back(cup);
-    }
-    let part2: i64 = cups.drain(..2).map(|i| i as i64).product();
-    dbg!(part2);
-}
-
-fn run(iterations: i32, cups: &mut VecDeque<i32>) {
-    let mut current_cup = *cups.front().unwrap();
-    let min = *cups.iter().min().unwrap();
-    let max = *cups.iter().max().unwrap();
-
-    for i in 0..iterations {
-        if (i % 1000) == 0 {
-            dbg!(i);
+    #[cfg(junk)]
+    {
+        let mut cups: VecDeque<i32> = input.iter().cloned().collect();
+        for i in 10..=1_000_000 {
+            cups.push_back(i);
         }
+        run(10_000_000, &mut cups);
 
-        assert_eq!(*cups.front().unwrap(), current_cup);
-
-        // get the next three cups after the current cup
-        while *cups.back().unwrap() != current_cup {
+        while *cups.back().unwrap() != 1 {
             let cup = cups.pop_front().unwrap();
             cups.push_back(cup);
         }
+        let part2: i64 = cups.drain(..2).map(|i| i as i64).product();
+        dbg!(part2);
+    }
+}
 
-        let this_round: VecDeque<_> = cups.drain(..3).collect();
+#[derive(Clone, Default, Debug)]
+struct Cup {
+    previous: usize,
+    next: usize,
+}
+
+fn setup_cups(input: &[usize]) -> Vec<Cup> {
+    let mut cups: Vec<Cup> = vec![Default::default(); input.len() + 1 /* because we never use 0 */];
+
+    for (&i, &j) in input.iter().zip(input.iter().skip(1)) {
+        cups[i].next = j;
+        cups[j].previous = i;
+    }
+    cups[input[0]].previous = *input.last().unwrap();
+    cups[*input.last().unwrap()].next = input[0];
+
+    cups
+}
+
+fn run(iterations: i32, input: &[usize]) -> Vec<usize> {
+    let mut current_cup = input[0];
+    let min = *input.iter().min().unwrap();
+    let max = *input.iter().max().unwrap();
+
+    let mut cups = setup_cups(input);
+
+    for _ in 0..iterations {
+        // splice out the next three after current_cup
+        // "range" is the three cups we're moving this round, inclusive
+        let beginning_of_range = cups[current_cup].next;
+        let mut end_of_range = current_cup;
+        let mut this_round = Vec::with_capacity(3);
+        for _ in 0..3 {
+            end_of_range = cups[end_of_range].next;
+            this_round.push(end_of_range);
+        }
+        let next_after_this_round = cups[end_of_range].next;
+        cups[current_cup].next = next_after_this_round;
+        cups[next_after_this_round].previous = current_cup;
+
         let mut destination = current_cup - 1;
         if destination < min {
             destination = max;
@@ -53,20 +75,21 @@ fn run(iterations: i32, cups: &mut VecDeque<i32>) {
             }
         }
 
-        rotate_to_put_at_end(destination, cups);
+        // now put the range back in between destination and its successor
+        let next_after_destination = cups[destination].next;
+        cups[destination].next = beginning_of_range;
+        cups[beginning_of_range].previous = destination;
+        cups[end_of_range].next = next_after_destination;
+        cups[next_after_destination].previous = end_of_range;
 
-        let mut this_round = this_round;
-        cups.append(&mut this_round);
-
-        rotate_to_put_at_end(current_cup, cups);
-
-        current_cup = *cups.front().unwrap();
+        current_cup = cups[current_cup].next;
     }
-}
 
-fn rotate_to_put_at_end(n: i32, cups: &mut VecDeque<i32>) {
-    let idx = cups.iter().position(|cup| cup == &n).unwrap();
-    let mut back_half = cups.split_off(idx + 1);
-    std::mem::swap(cups, &mut back_half);
-    cups.append(&mut back_half);
+    let mut ret = Vec::new();
+    let mut this = 1;
+    while cups[this].next != 1 {
+        ret.push(cups[this].next);
+        this = cups[this].next;
+    }
+    ret
 }
