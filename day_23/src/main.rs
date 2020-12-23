c4f5ac6946a85a576498676a31d6663b3e980f52
Matt Mullins <mmullins@mmlx.us>
2020-12-22 23:52:30 -0800

    day 23: use a single operation for rotating
    This is still running at the rate of about 3 seconds per thousand
    iterations, so it'd take upwards of two hours to run 10_000_000 times
    too :(

--- day_23/src/main.rs
+++ day_23/src/main.rs
@@ -29,14 +29,19 @@ fn run(iterations: i32, cups: &mut VecDeque<i32>) {
     let max = *cups.iter().max().unwrap();
 
     for i in 0..iterations {
-        dbg!(i);
+        if (i % 1000) == 0 {
+            dbg!(i);
+        }
+
+        assert_eq!(*cups.front().unwrap(), current_cup);
+
         // get the next three cups after the current cup
         while *cups.back().unwrap() != current_cup {
             let cup = cups.pop_front().unwrap();
             cups.push_back(cup);
         }
 
-        let this_round: Vec<_> = cups.drain(..3).collect();
+        let this_round: VecDeque<_> = cups.drain(..3).collect();
         let mut destination = current_cup - 1;
         if destination < min {
             destination = max;
@@ -48,22 +53,20 @@ fn run(iterations: i32, cups: &mut VecDeque<i32>) {
             }
         }
 
-        // now make sure the destination is at the end
-        while *cups.back().unwrap() != destination {
-            let cup = cups.pop_front().unwrap();
-            cups.push_back(cup);
-        }
+        rotate_to_put_at_end(destination, cups);
 
-        for cup in this_round {
-            cups.push_back(cup);
-        }
+        let mut this_round = this_round;
+        cups.append(&mut this_round);
 
-        // find the current cup again
-        while *cups.back().unwrap() != current_cup {
-            let cup = cups.pop_front().unwrap();
-            cups.push_back(cup);
-        }
+        rotate_to_put_at_end(current_cup, cups);
 
         current_cup = *cups.front().unwrap();
     }
+}
+
+fn rotate_to_put_at_end(n: i32, cups: &mut VecDeque<i32>) {
+    let idx = cups.iter().position(|cup| cup == &n).unwrap();
+    let mut back_half = cups.split_off(idx + 1);
+    std::mem::swap(cups, &mut back_half);
+    cups.append(&mut back_half);
 }
