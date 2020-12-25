4fabf95be1b33d6b96dbab04a73bc1825d0d96f2
Matt Mullins <mmullins@mmlx.us>
2020-12-24 21:28:42 -0800

    day 25: implement Baby-step Giant-step for DLP
    This was actually way more simple and runs way faster than I had
    expected.

--- day_25/src/main.rs
+++ day_25/src/main.rs
@@ -27,12 +27,45 @@ fn transform(subject: u64, loop_size: u64) -> u64 {
 }
 
 fn get_private_key(pubkey: u64) -> u64 {
-    for i in 0.. {
-        if transform(7, i) == pubkey {
-            return i;
+    let m = ((20201227 as f64).sqrt() + 1.0) as u64;
+
+    let table = (0..=m)
+        .map(|j| (transform(7, j), j))
+        .collect::<HashMap<_, _>>();
+
+    let (_q1, q2) = extended_euclidean(20201227, transform(7, m));
+    let inverse = q2.rem_euclid(20201227);
+
+    let mut y = pubkey;
+    for i in 0..m {
+        if let Some(j) = table.get(&y) {
+            return i * m + j;
         }
+        y *= inverse;
+        y %= 20201227;
     }
     panic!("could not find a suitable public key");
+}
+
+fn extended_euclidean(a: u64, b: u64) -> (u64, u64) {
+    extended_euclidean_inner(a, b, 1, 0, 0, 1)
+}
+
+fn extended_euclidean_inner(a: u64, b: u64, s0: u64, s1: u64, t0: u64, t1: u64) -> (u64, u64) {
+    if b == 0 {
+        assert_eq!(a, 1); // this algorithm makes no sense if gcd(orig_a, orig_b) != 1
+        return (s0, t0);
+    }
+
+    let q = &a / &b;
+    extended_euclidean_inner(
+        b.clone(),
+        a - &q * b,
+        s1.clone(),
+        s0 - &q * s1,
+        t1.clone(),
+        t0 - q * t1,
+    )
 }
 
 #[cfg(test)]
