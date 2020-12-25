19d6c5b4b86ef8c5b310e25e0a96808dba495a01
Matt Mullins <mmullins@mmlx.us>
2020-12-24 21:11:34 -0800

    day 25: naïve way to find the loop count
    This works for the example (since it's just 8 iterations) but is crazy
    slow on my input.

--- day_25/src/main.rs
+++ day_25/src/main.rs
@@ -8,6 +8,11 @@ fn do_main(filename: &str) {
     let keys = read_lines_from_file(filename)
         .map(|l| l.parse::<u64>().expect("not an integer"))
         .collect_vec();
+
+    let private_keys = keys
+        .iter()
+        .map(|&pubkey| get_private_key(pubkey))
+        .collect_vec();
 }
 
 fn transform(subject: u64, loop_size: u64) -> u64 {
@@ -21,10 +26,24 @@ fn transform(subject: u64, loop_size: u64) -> u64 {
     value
 }
 
+fn get_private_key(pubkey: u64) -> u64 {
+    for i in 0.. {
+        if transform(7, i) == pubkey {
+            return i;
+        }
+    }
+    panic!("could not find a suitable public key");
+}
+
 #[cfg(test)]
 mod test {
     #[test]
     fn transform() {
         assert_eq!(super::transform(7, 8), 5764801);
+    }
+
+    #[test]
+    fn get_private_key() {
+        assert_eq!(super::get_private_key(5764801), 8);
     }
 }
